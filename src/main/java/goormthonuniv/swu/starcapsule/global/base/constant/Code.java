08274950add9204d0e7995007357c53a99460154
hoyeon <aviationlovers@naver.com>
2024-09-26 23:58:55 +0900

    feat : Exception Handler

---
+++ src/main/java/goormthonuniv/swu/starcapsule/global/base/constant/Code.java
@@ -0,0 +1,73 @@
+package goormthonuniv.swu.starcapsule.global.base.constant;
+
+import goormthonuniv.swu.starcapsule.global.base.GeneralException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+@Getter
+@RequiredArgsConstructor
+public enum Code {
+
+    // 충돌 방지를 위한 Code format
+    // X1XXX: 제이
+    // X2XXX: 셀리나
+    // X3XXX: 메이슨
+    // ex) 메이슨이 닉네임 중복 에러코드를 만든다면
+    // USER_NICKNAME_DUPLICATED(13010, HttpStatus.BAD_REQUEST, "User nickname duplicated"),
+
+    OK(0, HttpStatus.OK, "Ok"),
+
+    BAD_REQUEST(10000, HttpStatus.BAD_REQUEST, "Bad request"),
+    VALIDATION_ERROR(10001, HttpStatus.BAD_REQUEST, "Validation error"),
+    NOT_FOUND(10002, HttpStatus.NOT_FOUND, "Requested resource is not found"),
+
+    INTERNAL_ERROR(20000, HttpStatus.INTERNAL_SERVER_ERROR, "Internal error"),
+    DATA_ACCESS_ERROR(20001, HttpStatus.INTERNAL_SERVER_ERROR, "Data access error"),
+
+    UNAUTHORIZED(40000, HttpStatus.UNAUTHORIZED, "User unauthorized");
+
+
+    private final Integer code;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public String getMessage(Throwable e) {
+        return this.getMessage(this.getMessage() + " - " + e.getMessage());
+        // 결과 예시 - "Validation error - Reason why it isn't valid"
+    }
+
+    public String getMessage(String message) {
+        return Optional.ofNullable(message)
+                .filter(Predicate.not(String::isBlank))
+                .orElse(this.getMessage());
+    }
+
+    public static Code valueOf(HttpStatus httpStatus) {
+        if (httpStatus == null) {
+            throw new GeneralException("HttpStatus is null.");
+        }
+
+        return Arrays.stream(values())
+                .filter(errorCode -> errorCode.getHttpStatus() == httpStatus)
+                .findFirst()
+                .orElseGet(() -> {
+                    if (httpStatus.is4xxClientError()) {
+                        return Code.BAD_REQUEST;
+                    } else if (httpStatus.is5xxServerError()) {
+                        return Code.INTERNAL_ERROR;
+                    } else {
+                        return Code.OK;
+                    }
+                });
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%d)", this.name(), this.getCode());
+    }
+}
---
+++ src/main/java/goormthonuniv/swu/starcapsule/global/base/dto/DataResponseDto.java
@@ -0,0 +1,32 @@
+package goormthonuniv.swu.starcapsule.global.base.dto;
+
+import goormthonuniv.swu.starcapsule.global.base.constant.Code;
+import lombok.Getter;
+
+@Getter
+public class DataResponseDto<T> extends ResponseDto{
+    private final T data;
+
+    private DataResponseDto(T data) {
+        super(true, Code.OK.getCode(), Code.OK.getMessage());
+        this.data = data;
+    }
+
+    private DataResponseDto(T data, String message) {
+        super(true, Code.OK.getCode(), message);
+        this.data = data;
+    }
+
+    public static <T> DataResponseDto<T> of(T data) {
+        return new DataResponseDto<>(data);
+    }
+
+    public static <T> DataResponseDto<T> of(T data, String message) {
+        return new DataResponseDto<>(data, message);
+    }
+
+    public static <T> DataResponseDto<T> empty() {
+        return new DataResponseDto<>(null);
+    }
+
+}
