f80ef195d0eccf2c749393f45b6b39c19d796b7d
hoyeon <aviationlovers@naver.com>
2024-10-17 02:45:59 +0900

    feat : 캘린더 추억 퍼즐 조회

--- src/main/java/goormthonuniv/swu/starcapsule/memory/MemoryRepository.java
+++ src/main/java/goormthonuniv/swu/starcapsule/memory/MemoryRepository.java
@@ -2,5 +2,9 @@ package goormthonuniv.swu.starcapsule.memory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface MemoryRepository extends JpaRepository<Memory, Long> {
+    boolean existsBySnowball_IdAndCreatedAtBetween(Long snowballId, LocalDateTime startOfDay, LocalDateTime endOfDay);
 }
+
