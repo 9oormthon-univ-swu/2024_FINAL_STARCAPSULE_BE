152712d922f456d0d3b0a97aaf5be9728dc277de
hoyeon <aviationlovers@naver.com>
2024-10-27 18:05:40 +0900

    fix : TimeZone 일괄 수정

--- src/main/java/goormthonuniv/swu/starcapsule/memory/MemoryResponse.java
+++ src/main/java/goormthonuniv/swu/starcapsule/memory/MemoryResponse.java
@@ -2,6 +2,8 @@ package goormthonuniv.swu.starcapsule.memory;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class MemoryResponse {
     private Long id;
@@ -10,14 +12,15 @@ public class MemoryResponse {
     private String imageUrl;
     private String writer;
     private String object;
+    private LocalDateTime createdAt;
 
-    public MemoryResponse(Memory memory){
+    public MemoryResponse(Memory memory) {
         this.id = memory.getId();
         this.title = memory.getTitle();
         this.answer = memory.getAnswer();
         this.imageUrl = memory.getImageUrl();
         this.writer = memory.getWriter();
         this.object = memory.getMemoryObjectShape().getObjectShapeName();
+        this.createdAt = memory.getCreatedAt();
     }
-
 }
