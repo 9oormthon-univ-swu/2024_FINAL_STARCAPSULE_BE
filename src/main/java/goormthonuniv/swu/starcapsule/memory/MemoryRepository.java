290b1913a7c8bf187569d8e0350ab3fe8619fa30
hoyeon <aviationlovers@naver.com>
2024-10-31 06:25:43 +0900

    fix : 해당 날짜만 조회하도록 해결

--- src/main/java/goormthonuniv/swu/starcapsule/memory/MemoryRepository.java
+++ src/main/java/goormthonuniv/swu/starcapsule/memory/MemoryRepository.java
@@ -10,10 +10,11 @@ import java.util.List;
 public interface MemoryRepository extends JpaRepository<Memory, Long> {
     boolean existsBySnowball_IdAndCreatedAtBetween(Long snowballId, LocalDateTime startOfDay, LocalDateTime endOfDay);
 
-    @Query("SELECT m FROM Memory m WHERE m.createdAt BETWEEN :startDateTime AND :endDateTime AND m.snowball.id = :snowballId")
+    @Query("SELECT m FROM Memory m WHERE m.createdAt >= :startDateTime AND m.createdAt < :endDateTime AND m.snowball.id = :snowballId")
     List<Memory> findMemoriesByCreatedAtBetweenAndSnowballId(
             @Param("startDateTime") LocalDateTime startDateTime,
             @Param("endDateTime") LocalDateTime endDateTime,
             @Param("snowballId") Long snowballId);
+
 }
 
