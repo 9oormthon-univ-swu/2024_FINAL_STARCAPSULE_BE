bf294b4fb3938df38cd94aa369e6a9a3ece65ab5
hoyeon <aviationlovers@naver.com>
2024-11-06 20:05:32 +0900

    fix : 인증토큰 추가

--- src/main/java/goormthonuniv/swu/starcapsule/memory/MemoryController.java
+++ src/main/java/goormthonuniv/swu/starcapsule/memory/MemoryController.java
@@ -2,6 +2,8 @@ package goormthonuniv.swu.starcapsule.memory;
 
 import goormthonuniv.swu.starcapsule.global.template.BaseResponse;
 import goormthonuniv.swu.starcapsule.snowball.SnowballResponse;
+import goormthonuniv.swu.starcapsule.user.User;
+import goormthonuniv.swu.starcapsule.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -23,6 +25,8 @@ import java.io.IOException;
 @Tag(name = "함께하는 기록", description = "타인이 나의 페이지의 추억을 기록하는 API")
 public class MemoryController {
     private final MemoryService memoryService;
+    private final UserService userService;
+
     @Operation(summary = "함께하는 기록", description = "타인이 나의 페이지의 추억을 기록합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "함께하는 기록 작성 성공",
@@ -49,13 +53,30 @@ public class MemoryController {
                     content = @Content(schema = @Schema(implementation = SnowballResponse.class))),
             @ApiResponse(responseCode = "400", description = "잘못된 요청",
                     content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+            @ApiResponse(responseCode = "401", description = "인증 실패",
+                    content = @Content(schema = @Schema(implementation = BaseResponse.class))),
             @ApiResponse(responseCode = "404", description = "요청한 memory를 찾을 수 없음",
                     content = @Content(schema = @Schema(implementation = BaseResponse.class)))
     })
     @GetMapping("/{user_id}/{memory_id}")
-    public ResponseEntity<?> getDetailMemory(@PathVariable("user_id") String userId,
-                                             @PathVariable("memory_id") Long memoryId) throws IOException {
+    public ResponseEntity<?> getDetailMemory(
+            @RequestHeader("Authorization") String token,
+            @PathVariable("user_id") String userId,
+            @PathVariable("memory_id") Long memoryId) throws IOException {
+
+        User user = userService.findByAccessToken(token);
+
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(BaseResponse.response("로그인 후 이용해주세요."));
+        }
+
         Memory memory = memoryService.getMemory(userId, memoryId);
+
+        if (memory == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(BaseResponse.response("해당 추억을 찾을 수 없습니다."));
+        }
         return ResponseEntity.ok(BaseResponse.response(new MemoryResponse(memory)));
     }
 
