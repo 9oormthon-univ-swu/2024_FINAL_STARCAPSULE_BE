52bf5d9fd72bdc60e0457da9ee99222def887518
hoyeon <aviationlovers@naver.com>
2024-11-17 01:50:54 +0900

    Fix: LocalDateTime으로 변경

--- src/main/java/goormthonuniv/swu/starcapsule/dailyQuestion/DailyQuestionRepository.java
+++ src/main/java/goormthonuniv/swu/starcapsule/dailyQuestion/DailyQuestionRepository.java
@@ -2,11 +2,9 @@ package goormthonuniv.swu.starcapsule.dailyQuestion;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface DailyQuestionRepository extends JpaRepository<DailyQuestion ,Long> {
-    Optional<DailyQuestion> findByDate(LocalDate date);
-
+    Optional<DailyQuestion> findByDate(LocalDateTime date);
 }
