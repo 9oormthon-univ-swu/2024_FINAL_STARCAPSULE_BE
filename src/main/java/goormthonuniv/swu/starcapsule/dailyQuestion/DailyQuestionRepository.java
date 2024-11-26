fde251ee9f5519cdd0c0cdd4fc0db59b0c50aebe
joungeun0825 <joungeun0825@gmail.com>
2024-11-26 20:14:33 +0900

    fix: 시간 날짜만 비교하게 수정

--- src/main/java/goormthonuniv/swu/starcapsule/dailyQuestion/DailyQuestionRepository.java
+++ src/main/java/goormthonuniv/swu/starcapsule/dailyQuestion/DailyQuestionRepository.java
@@ -1,11 +1,14 @@
 package goormthonuniv.swu.starcapsule.dailyQuestion;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface DailyQuestionRepository extends JpaRepository<DailyQuestion ,Long> {
-    Optional<DailyQuestion> findByDate(LocalDate date);
+public interface DailyQuestionRepository extends JpaRepository<DailyQuestion, Long> {
+    @Query("SELECT dq FROM DailyQuestion dq WHERE DATE(dq.date) = :date")
+    Optional<DailyQuestion> findByDate(@Param("date") LocalDate date);
 }
