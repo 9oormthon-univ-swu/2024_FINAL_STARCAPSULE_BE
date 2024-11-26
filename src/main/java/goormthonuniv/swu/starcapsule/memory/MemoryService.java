9d9aa716806d2ef545e6b9774d621845d19056a9
joungeun0825 <joungeun0825@gmail.com>
2024-11-26 20:55:09 +0900

    fix: json 새 키

--- src/main/java/goormthonuniv/swu/starcapsule/memory/MemoryService.java
+++ src/main/java/goormthonuniv/swu/starcapsule/memory/MemoryService.java
@@ -64,7 +64,7 @@ public class MemoryService {
     }
 
     public String getPublicUrl(MultipartFile image) throws IOException {
-        ClassPathResource resource = new ClassPathResource("snowballlog-442904-ffaee9fa7eda.json");
+        ClassPathResource resource = new ClassPathResource("snowballlog-442904-83d49811a181.json");
 
         InputStream credentialsStream = resource.getInputStream();
         GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream);
